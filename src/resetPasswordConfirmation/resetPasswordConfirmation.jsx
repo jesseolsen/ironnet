c15472220faee6d33ac986d67a4bf1db74afdf00
Jesse Olsen <jesse.olsen@hpe.com>
2021-02-11 09:12:49 -0700

    Make final dialog dismissible

--- src/resetPasswordConfirmation/resetPasswordConfirmation.jsx
+++ src/resetPasswordConfirmation/resetPasswordConfirmation.jsx
@@ -1,66 +1,205 @@
-
 import React from "react";
 // import PropTypes from "prop-types";
 
 import "./resetPasswordConfirmation.css";
 
 class ResetPasswordConfirmation extends React.Component {
-
   constructor(props) {
-      super(props);
-      this.state = {
-
-      };
+    super(props);
+    this.state = {};
   }
 
   render() {
-
     return (
-          <div data-layer="eace64b3-f1c2-4cf1-b68e-2bca83266a6f" className="resetPasswordConfirmation">        <div data-layer="66204e7c-8583-4246-809c-cab61be6aef8" className="signIn6eddf2e8">            <div data-layer="35ee9749-3a22-4d6e-8170-5d2f1343caa3" className="bg532b8fd6"></div>
-            <div data-layer="2094cf2c-e058-46ee-bfa7-daffe18033bb" className="xCopyrightAcme2020">© Copyright ACME 2020</div>
-            <div data-layer="506b0469-7c89-4618-8fda-01f4c5da2e46" className="content">                <div data-layer="3bd0e4f9-f23a-4116-9148-a4517c306b3b" className="forgotYourPasswordResetItHere"><span className="forgotYourPasswordResetItHere-0">Forgot your password? </span><span className="forgotYourPasswordResetItHere-22">Reset it here</span><span className="forgotYourPasswordResetItHere-35">.</span></div>
-                <div data-layer="64925271-f975-43cd-b6fb-35ec4b7acccc" className="button">                    <div data-layer="ae00cf59-9a5b-4a64-b26b-ceb0e6fda798" className="buttonBg"></div>
-                    <div data-layer="94417e37-b3ff-4229-a4a9-350173139bf0" className="signIn2dc6fda6">Sign In</div>
-</div>
-                <div data-layer="eb2c9e08-181b-43b2-9580-76489b076330" className="inputd12cf21e">                    <div data-layer="7e55c285-408e-4c32-8415-2c06ad82ef97" className="bgc43139ab"></div>
-                    <div data-layer="dc1cba26-edda-4b09-8a8d-9753c8b02ab1" className="password">Password</div>
-</div>
-                <div data-layer="3115f54a-1455-49bb-932f-444ce4a7d448" className="input">                    <div data-layer="f8af9114-558a-4ddd-826d-09026cc5e747" className="bg16362a48"></div>
-                    <div data-layer="ba9b11e5-ee22-40ef-aead-557de1fd1ceb" className="email">Email</div>
-</div>
-                <svg data-layer="45e41d40-9e62-4d6a-bd62-6062eb01653d" preserveAspectRatio="none" viewBox="-2.5 -2.5 43 5" className="line1"><path d="M 0 0 L 38 0"  /></svg>
-                <div data-layer="c78938d6-7fe8-4159-9268-8bd9a15a8a63" className="signIn">Sign In</div>
-</div>
-            <div data-layer="2f3baadd-6ebe-48d6-911d-5eb8fff71730" className="toast">                <div data-layer="0f77ad6b-e0f9-44c7-b0b1-c7967a27f753" className="bg"></div>
-                <div data-layer="a04f2eb4-62fc-4b79-a7b6-fc051c63ef9d" className="ctaBg"></div>
-                <div data-layer="3e1155e0-46db-486a-b175-768fcd4b66b8" className="x458c11b8">👍</div>
-                <div data-layer="8b167b0e-086d-410d-9428-9dacd07bf471" className="passwordResetInstructionsHaveBeenSent">Password reset instructions have been sent.</div>
-                <div data-layer="eee1efce-21c9-488c-9a42-958e8dc15f80" className="thumbsUp"></div>
-</div>
-</div>
-        <div data-layer="12a14045-8385-4175-9c78-aeb62fbd8900" className="cta">            <div data-layer="23f53c3a-82da-4c15-9ebe-e4b5b270fb07" className="background">                <div data-layer="a0b4475a-2416-4e0e-9d11-d3b7d4cf74b3" className="gradient"></div>
-                <div data-layer="f1b96cdb-bdf4-4d77-ac7d-f7e927e2a773" className="accent9dd25470"></div>
-                <div data-layer="bd5f0740-7c6f-44de-908c-69dce68c8f9f" className="accent"></div>
-</div>
-            <div data-layer="51874084-f902-482a-9d10-d94c88e8a088" className="signInAndStartSavingYourTimeToday">Sign in and start saving your time today.</div>
-            <div data-layer="55637a5d-a7fe-41bd-988d-cbe8b6a6330c" className="weProvide247SupportWithOurHighlyDedicatedProfessionalTeam">We provide 24/7 support with our highly dedicated professional team.</div>
-            <div data-layer="1a58eb45-6ca9-4a71-8101-624276658d3b" className="logo">                <div data-layer="24024dfe-9d90-48b3-bc7c-837b2d7e64b1" className="acme">ACME</div>
-                <div data-layer="e2625d97-6d6f-45be-835f-7d34670afdaf" className="rectangle1"></div>
-</div>
-</div>
-</div>
-
+      <div
+        data-layer="eace64b3-f1c2-4cf1-b68e-2bca83266a6f"
+        className="resetPasswordConfirmation"
+      >
+        {" "}
+        <div
+          data-layer="66204e7c-8583-4246-809c-cab61be6aef8"
+          className="signIn6eddf2e8"
+        >
+          {" "}
+          <div
+            data-layer="35ee9749-3a22-4d6e-8170-5d2f1343caa3"
+            className="bg532b8fd6"
+          ></div>
+          <div
+            data-layer="2094cf2c-e058-46ee-bfa7-daffe18033bb"
+            className="xCopyrightAcme2020"
+          >
+            © Copyright ACME 2020
+          </div>
+          <div
+            data-layer="506b0469-7c89-4618-8fda-01f4c5da2e46"
+            className="content"
+          >
+            {" "}
+            <div
+              data-layer="3bd0e4f9-f23a-4116-9148-a4517c306b3b"
+              className="forgotYourPasswordResetItHere"
+            >
+              <span className="forgotYourPasswordResetItHere-0">
+                Forgot your password?{" "}
+              </span>
+              <span className="forgotYourPasswordResetItHere-22">
+                Reset it here
+              </span>
+              <span className="forgotYourPasswordResetItHere-35">.</span>
+            </div>
+            <div
+              data-layer="64925271-f975-43cd-b6fb-35ec4b7acccc"
+              className="button"
+            >
+              {" "}
+              <div
+                data-layer="ae00cf59-9a5b-4a64-b26b-ceb0e6fda798"
+                className="buttonBg"
+              ></div>
+              <div
+                data-layer="94417e37-b3ff-4229-a4a9-350173139bf0"
+                className="signIn2dc6fda6"
+              >
+                Sign In
+              </div>
+            </div>
+            <div
+              data-layer="eb2c9e08-181b-43b2-9580-76489b076330"
+              className="inputd12cf21e"
+            >
+              {" "}
+              <div
+                data-layer="7e55c285-408e-4c32-8415-2c06ad82ef97"
+                className="bgc43139ab"
+              ></div>
+              <div
+                data-layer="dc1cba26-edda-4b09-8a8d-9753c8b02ab1"
+                className="password"
+              >
+                Password
+              </div>
+            </div>
+            <div
+              data-layer="3115f54a-1455-49bb-932f-444ce4a7d448"
+              className="input"
+            >
+              {" "}
+              <div
+                data-layer="f8af9114-558a-4ddd-826d-09026cc5e747"
+                className="bg16362a48"
+              ></div>
+              <div
+                data-layer="ba9b11e5-ee22-40ef-aead-557de1fd1ceb"
+                className="email"
+              >
+                Email
+              </div>
+            </div>
+            <svg
+              data-layer="45e41d40-9e62-4d6a-bd62-6062eb01653d"
+              preserveAspectRatio="none"
+              viewBox="-2.5 -2.5 43 5"
+              className="line1"
+            >
+              <path d="M 0 0 L 38 0" />
+            </svg>
+            <div
+              data-layer="c78938d6-7fe8-4159-9268-8bd9a15a8a63"
+              className="signIn"
+            >
+              Sign In
+            </div>
+          </div>
+          <a href='#/'>
+            <div
+              data-layer="2f3baadd-6ebe-48d6-911d-5eb8fff71730"
+              className="toast"
+            >
+              {" "}
+              <div
+                data-layer="0f77ad6b-e0f9-44c7-b0b1-c7967a27f753"
+                className="bg"
+              ></div>
+              <div
+                data-layer="a04f2eb4-62fc-4b79-a7b6-fc051c63ef9d"
+                className="ctaBg"
+              ></div>
+              <div
+                data-layer="3e1155e0-46db-486a-b175-768fcd4b66b8"
+                className="x458c11b8"
+              >
+                👍
+              </div>
+              <div
+                data-layer="8b167b0e-086d-410d-9428-9dacd07bf471"
+                className="passwordResetInstructionsHaveBeenSent"
+              >
+                Password reset instructions have been sent.
+              </div>
+              <div
+                data-layer="eee1efce-21c9-488c-9a42-958e8dc15f80"
+                className="thumbsUp"
+              ></div>
+            </div>
+          </a>
+        </div>
+        <div data-layer="12a14045-8385-4175-9c78-aeb62fbd8900" className="cta">
+          {" "}
+          <div
+            data-layer="23f53c3a-82da-4c15-9ebe-e4b5b270fb07"
+            className="background"
+          >
+            {" "}
+            <div
+              data-layer="a0b4475a-2416-4e0e-9d11-d3b7d4cf74b3"
+              className="gradient"
+            ></div>
+            <div
+              data-layer="f1b96cdb-bdf4-4d77-ac7d-f7e927e2a773"
+              className="accent9dd25470"
+            ></div>
+            <div
+              data-layer="bd5f0740-7c6f-44de-908c-69dce68c8f9f"
+              className="accent"
+            ></div>
+          </div>
+          <div
+            data-layer="51874084-f902-482a-9d10-d94c88e8a088"
+            className="signInAndStartSavingYourTimeToday"
+          >
+            Sign in and start saving your time today.
+          </div>
+          <div
+            data-layer="55637a5d-a7fe-41bd-988d-cbe8b6a6330c"
+            className="weProvide247SupportWithOurHighlyDedicatedProfessionalTeam"
+          >
+            We provide 24/7 support with our highly dedicated professional team.
+          </div>
+          <div
+            data-layer="1a58eb45-6ca9-4a71-8101-624276658d3b"
+            className="logo"
+          >
+            {" "}
+            <div
+              data-layer="24024dfe-9d90-48b3-bc7c-837b2d7e64b1"
+              className="acme"
+            >
+              ACME
+            </div>
+            <div
+              data-layer="e2625d97-6d6f-45be-835f-7d34670afdaf"
+              className="rectangle1"
+            ></div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-ResetPasswordConfirmation.propTypes = {
+ResetPasswordConfirmation.propTypes = {};
 
-}
-
-ResetPasswordConfirmation.defaultProps = {
-
-}
-
+ResetPasswordConfirmation.defaultProps = {};
 
 export default ResetPasswordConfirmation;
